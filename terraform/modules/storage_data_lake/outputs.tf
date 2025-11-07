626ec1ef3c091d56daab066d5fb77ba188481be7
bass224 <abdoulbassitydiallo.com>
2025-11-07 13:38:23 +0100

    fix: module container

--- terraform/modules/storage_data_lake/outputs.tf
+++ terraform/modules/storage_data_lake/outputs.tf
@@ -1,10 +1,10 @@
 
-output "sotrage_account_name" {
+output "storage_account_name" {
   description = "Le nom du compte de stockage crée"
   value = azurerm_storage_account.stg.name
 }
 
-output "sotrage_account_id" {
+output "storage_account_id" {
   description = "l'id du compte de stockage"
   value = azurerm_storage_account.stg.id
 }
