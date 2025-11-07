d487977d05841d0560ce61fd444b232403d1cef5
bass224 <abdoulbassitydiallo.com>
2025-11-07 23:38:22 +0100

    feat : création de la database

--- terraform/modules/database/outputs.tf
+++ terraform/modules/database/outputs.tf
@@ -4,3 +4,9 @@ output "sql_server_name" {
   description = "Nom du server sql crée"
   value = azurerm_mssql_server.sql_server.name
 }
+
+
+output "server_id" {
+  description = "l'id de la database crée"
+  value = azurerm_mssql_database.sql_server.id
+}
