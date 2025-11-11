8c84c1f70b8f703c6a6a37abc321554485f1a50e
bass224 <abdoulbassitydiallo.com>
2025-11-11 14:50:38 +0100

    Donner l'identité managée à ADF sinon il pourra pas accéder aux ressources quand on créera les link service et autres

--- terraform/modules/data_factory/factory.tf
+++ terraform/modules/data_factory/factory.tf
@@ -5,5 +5,11 @@ resource "azurerm_data_factory" "mydatafact" {
   name =var.datafactory_name
   location = var.location
   resource_group_name = var.resource_group_name
+  
+  # Donner l'identité managée à ADF sinon il ne pourra pas accéder aux autres ressources comme ADLS Gen2
+  # Cela demande à Azure d’attacher une identité managée (service principal) à notre ADF.
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
