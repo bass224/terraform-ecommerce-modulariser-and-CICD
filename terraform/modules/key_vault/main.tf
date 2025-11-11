0fce710a16106b5a5e656da0b79bcbc46b1b58aa
bass224 <abdoulbassitydiallo.com>
2025-11-11 11:02:15 +0100

    fix : ajout du role rbac au lieu de policies au keyvault

--- terraform/modules/key_vault/main.tf
+++ terraform/modules/key_vault/main.tf
@@ -12,4 +12,7 @@ resource "azurerm_key_vault" "kv" {
   location = var.location
   sku_name = "standard"
   tenant_id = data.azurerm_client_config.current.tenant_id
+  #pas obligatoire mais très utile sinon on aura pas le role RBAC et ça va crée des pb si on essaye de crée des secrets 
+  #car le role sera en policies et il ignorera les roles nécessaires pour la création d'un secrets 
+  enable_rbac_authorization   = true   # RBAC (recommandé)
 }
